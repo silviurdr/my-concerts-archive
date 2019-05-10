ba2c035c32845a4d1d8ca8f309a4be72886ede00
Silviu Kndr <silviu.catrina@gmail.com>
2019-05-10 12:52:17 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-concerts-archive/precache-manifest.ae508ccf2780992e787f2995de8d0c1b.js"
+  "/my-concerts-archive/precache-manifest.c462aa2bc24a968fbf0c2d55e892980c.js"
 );
 
 workbox.clientsClaim();
