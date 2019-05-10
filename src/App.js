7eb326865bb72f1440b7639baff99690399ff50a
Silviu Kndr <silviu.catrina@gmail.com>
2019-05-10 11:33:55 +0300

    hashrou

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Concertos from "./components/concertos";
 import Timeline from "./components/pages/timeline";
 import "font-awesome/css/font-awesome.min.css";
@@ -9,12 +9,12 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 class App extends Component {
   render() {
     return (
-      <Router>
+      <HashRouter>
         <Switch>
           <Route exact path="/" component={Concertos} />
           <Route path="/timeline" component={Timeline} />
         </Switch>
-      </Router>
+      </HashRouter>
     );
   }
 }
