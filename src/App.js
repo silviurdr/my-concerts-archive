b69a84bb972d5aef771b14bc6cf44bb9836f5c8f
Silviu Kndr <silviu.catrina@gmail.com>
2019-05-10 11:04:02 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Concertos from "./components/concertos";
 import Timeline from "./components/pages/timeline";
 import "font-awesome/css/font-awesome.min.css";
@@ -10,8 +10,10 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Route exact path="/" component={Concertos} />
-        <Route path="/timeline" component={Timeline} />
+        <Switch>
+          <Route exact path="/" component={Concertos} />
+          <Route path="/timeline" component={Timeline} />
+        </Switch>
       </Router>
     );
   }
